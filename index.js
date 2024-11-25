19084db81371e6ab8a556b2023e030ac79c986fe
ju3tin <ju3tin@hotmail.co.uk>
2024-11-25 21:22:43 +0000

    sdf

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require("connectMongo");
+const connectDB = require("./connectMongo");
 
 //const mongoose = require('mongoose');
 
